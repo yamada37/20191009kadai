39715942aa6626cffb7548699fa36e19a24c2c85
yamada37 <mina.375.yp@gmail.com>
2019-10-23 05:16:20 +0000

    Some fix

--- kadai-1/index.js
+++ kadai-1/index.js
@@ -1,1 +1,7 @@
 console.log('bye')
+const name = 'mina'
+console.log(name)
+let name2 = 'kana'
+console.log(name2)
+name2 = 'rinka'
+console.log(name2)
---
+++ kadai-2/index.js
@@ -0,0 +1,14 @@
+const firstName = 'Mina'
+const familyName = 'Yamada'
+console.log(firstName + familyName)
+
+const sum = 1+2+3+4+5+6+7+8+9+10
+console.log(sum)
+
+const hasDog = false
+const hasCat = true
+
+console.log(hasDog || hasCat)
+console.log(hasDog && hasCat)
+//犬も猫も飼っていない
+console.log(!hasDog && !hasCat)
